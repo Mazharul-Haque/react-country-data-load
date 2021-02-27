a9cc9470df8490ca58056d44777f903251dc3ed2
Mazharul-Haque <mshagor009@gmail.com>
2021-02-27 21:13:30 +0600

    country information load from api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import Country from './components/Country/Country';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    fetch('https://restcountries.eu/rest/v2/all')
+      .then(res => res.json())
+      .then(data => setCountries(data))
+      .catch(error => console.log(error))
+
+  }, [])
   return (
     <div className="App">
+      <h1>Country loaded : {countries.length}</h1>
+      <ul>
+        { 
+          countries.map(country => <Country
+             country ={country} 
+             key ={country.alpha3Code} 
+             
+             ></Country> )
+        }
+      </ul>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/Country/Country.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Country = (props) => {
+    const {name,population,region,flag} = props.country;
+    const flagStyle ={height : '50px'}
+
+    return (
+        <div>
+            <h4>This is a {name}</h4>
+            <img style={flagStyle} src={flag} alt=""/>
+            <p>population :{population}</p>
+            <p>region :{region}</p>
+        </div>
+    );
+};
+
+export default Country;
